a7a18d6d13693d25ec78d1f82b222d6867cf2492
Mxrshmxllow <rlawenko95@gmail.com>
2021-05-03 11:53:27 +0800

    3 files modified, 2 added, 2 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,7 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { CalculatorPage } from '../pages/calculator/calculator';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -12,7 +12,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    CalculatorPage,
     HomePage,
     TabsPage
   ],
@@ -24,7 +24,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    CalculatorPage,
     HomePage,
     TabsPage
   ],
--- pages/calculator/calculator.ts
+++ pages/calculator/calculator.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-calculator',
+  templateUrl: 'calculator.html'
 })
-export class ContactPage {
+export class CalculatorPage {
 
   constructor(public navCtrl: NavController) {
 
