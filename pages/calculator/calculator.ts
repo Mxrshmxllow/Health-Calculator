286a5377934e36c0768438f9d3fd60895df79978
Mxrshmxllow <rlawenko95@gmail.com>
2021-05-03 14:36:11 +0800

    2 files modified

--- pages/calculator/calculator.ts
+++ pages/calculator/calculator.ts
@@ -9,36 +9,47 @@ import { NavController } from 'ionic-angular';
 export class CalculatorPage {
   feet: number;
   inches: number;
+  totalinches: number;
   robinson: number;
   miller: number;
   hamwi: number;
   devine: number;
   gender: string;
   message: string;
+  genderstring: string;
 
   constructor(public navCtrl: NavController) {
 
   }
 calculateIBW(){
+  this.totalinches = ((this.feet-5)*12)+1*this.inches;
+
+  if (this.gender == "m" && this.feet >= 5){
+    this.genderstring = "Male"
+    this.robinson = 52 + (1.9*this.totalinches)
+    this.miller = 56.2 + (1.41*this.totalinches)
+    this.hamwi = 48 + (2.7*this.totalinches)
+    this.devine = 50 + (2.3*this.totalinches)
+  }
+  else if (this.gender == "f" && this.feet >= 5){
+    this.genderstring = "Female"
+    this.robinson = 49 + (1.7*this.totalinches)
+    this.miller = 53.1 + (1.36*this.totalinches)
+    this.hamwi = 45.5 + (2.2*this.totalinches)
+    this.devine = 45.5 + (2.3*this.totalinches)
+  }
+  else {
+    this.genderstring == "Error"
+    this.robinson = 0
+    this.miller = 0
+    this.hamwi = 0
+    this.devine = 0
+  }
+
   this.robinson = parseFloat(this.robinson.toFixed(2))
   this.miller = parseFloat(this.miller.toFixed(2))
   this.hamwi = parseFloat(this.hamwi.toFixed(2))
   this.devine = parseFloat(this.devine.toFixed(2))
-
-  if (this.feet = 5, this.gender ="m"){
-    this.robinson = 52 + (1.9*this.inches)
-    this.miller = 56.2 + (1.41*this.inches)
-    this.hamwi = 48 + (2.7*this.inches)
-    this.devine = 50 + (2.3*this.inches)
-  }
-  else if (this.feet = 5, this.gender ="f"){
-    this.robinson = 49 + (1.7*this.inches)
-    this.miller = 53.1 + (1.36*this.inches)
-    this.hamwi = 45.5 + (2.2*this.inches)
-    this.devine = 45.5 + (2.3*this.inches)
-  }
-
-  this.message = "Robinson (1983): " + this.robinson + "\nMiller (1983): " + this.miller + "\nHamwi (1964): " + this.hamwi + "Devine (1974): " + this.devine;
 
 
   }
